0c6545c4079951952f5960e316dfad7a60823098
Mykola <nnv2006@ukr.net>
2024-03-08 01:54:24 +0200

    UPD service

--- src/services/ShopAPI.js
+++ src/services/ShopAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const BASE_URL = 'https://med-deliv-test-back.onrender.com/api/';
-const BASE_URL = 'http://localhost:5000/api/';
+const BASE_URL = 'https://med-deliv-test-back.onrender.com/api/';
+// const BASE_URL = 'http://localhost:5000/api/';
 
 const productsApi = axios.create({
     baseURL: BASE_URL,
